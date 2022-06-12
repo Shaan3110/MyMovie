35b51196795f823b458313a13bcaead4a21f259e
Shaan3110 <hitarasigan0987654321@gmail.com>
2022-06-12 23:17:57 +0530

    Home autoplayer done

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import AutoPlayer from "../../components/AutoPlayer/AutoPlayer";
 import Header from "../../components/Header/Landing/Header";
 
 function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <AutoPlayer />
+    </>
+  );
 }
 
 export default Home;
